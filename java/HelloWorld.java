92526a7fd8101cce73816505233b789dc5265650
Liyana <nliyanaam@smu.edu.sg>
2021-06-11 14:41:20 +0800

    java practice

---
+++ java/HelloWorld.java
@@ -0,0 +1,14 @@
+package java;
+public class HelloWorld{
+    public static void main (String[] args) {
+        System.out.println("Hello World");
+        System.out.print("I'm so happy...");
+        System.out.println("To meet you!");
+    }
+}
+
+/*
+We need to:
+execute with the command: java <file>
+compile with the command: javac <file.java>
+*/
