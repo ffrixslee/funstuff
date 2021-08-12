70685e52fdebc83998da12427b3c99441dd02561
Liyana <69780620+ffrixslee@users.noreply.github.com>
2021-08-12 17:05:42 +0800

    update

--- nodeProj/routes.js
+++ nodeProj/routes.js
@@ -1,11 +1,38 @@
 const http = require("http");
 
 const host = 'localhost';
-const port = 8000;
+const port = 8080;
 
-const requestListener = function (req, res) {};
+const books = JSON.stringify([
+    { title: "The Alchemist", author: "Paulo Coelho", year: 1988 },
+    { title: "Twilight", author: "Stephenie Meyer", year: 2008}
+]);
+
+const authors = JSON.stringify([
+    { name: "Paulo Coelho", countryOfBirth: "Brazil", yearOfBirth: 19487 },
+    { name: "Kahlil Gibran", countryOfBirth: "Lebanon", yearOfBirth: 1883 }
+]);
+
+
+const requestListener = function (req, res) {
+    res.setHeader("Content-Type", "application/json");
+    switch (req.url) {
+        case "/books":
+            res.writeHead(200);
+            res.end(books);
+            break
+        case "/authors":
+            res.writeHead(200);
+            res.end(authors);
+            break
+        default:
+            res.writeHead(404);
+            res.end(JSON.stringify({error:"Resource not found"}));
+    }
+};
 
 const server = http.createServer(requestListener);
 server.listen(port, host, () => {
-    console.log('Server is running on http://${host}:${port}');
+    console.log(`Server is running on http://${host}:${port}`);
 });
+
